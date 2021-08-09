e286b00f4b054c607c58ff318171536d446d6a2b
lev-stack <boyarskij-2000@mail.ru>
2021-08-09 21:14:03 +0300

    JS

---
+++ Task 19/app.js
@@ -0,0 +1,11 @@
+var a = 1;
+var b = 1/2;
+var c = 1/3;
+var d = 1/4;
+var f = 4
+console.log (f*d)
+console.log (f*c)
+console.log (f*b)
+console.log (f*a)
+console.log (f*c)
+console.log (((f*c)(f*b)(f*d)))
---
+++ Task 20/user.js
@@ -0,0 +1,22 @@
+var UserName = "Вася";
+const UserSurname = "Пупкин";
+
+var Height, weight;
+
+Height =  180, weight = 0.89;
+
+var oldweight = weight
+
+var weight = 89;
+
+console.log("Меня зовут " + UserName + " " + UserSurname + ". Я вешу " + weight + " ц. Мой рост - " + Height + "см.");
+
+weight = weight + 10/100;
+Height -= 2;
+
+console.log(UserName + " " + UserSurname + " потолстел и стоптался ");
+
+UserName = "Петя"
+weight *=2
+
+console.log("Теперь я " + UserName + " " + UserSurname + " и вешу в " +( weight - oldweight ) + " раз. Больше, чем в молодости.");
